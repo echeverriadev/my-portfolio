f15d9c1c5e25e67ec5ed95e97fd790a1e9f97b34
Carlos Echeverria <echeverriadev@gmail.com>
2022-08-15 17:17:59 -0500

    feature: redirect to rrss buttons

--- components/landing/index.vue
+++ components/landing/index.vue
@@ -1,3 +1,3 @@
 <template src="./landing.html"></template>
-<script src="./landing.ts"></script>
+<script src="./landing.ts" lang="ts"></script>
 <style src="./landing.scss" scoped lang="scss"></style>
---
+++ components/landing/landing.ts
@@ -0,0 +1,8 @@
+import Catalog from '@/components/shared/catalog/index.vue'
+
+export default {
+  name: 'Landing',
+  components: {
+    Catalog,
+  },
+}
